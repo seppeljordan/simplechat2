b6a8b10d5c9d741ac92b9f71cc36e125f260056c
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2015-03-27 02:29:42 +0100

    Implement who command

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,23 @@
+import Control.Concurrent
+import Control.Concurrent.Async
+import qualified Control.Event.Handler as Handler
+import Control.Monad hiding
+    ( mapM_ )
 import Data.Foldable
+import Data.Monoid
 import Network.Socket
-import Reactive.Banana
-import Reactive.Banana.Frameworks hiding
-    ( register )
 import Prelude hiding
     ( mapM_
     , foldl
     )
-import Control.Monad hiding
-    ( mapM_ )
-import Network.Socket
-import Control.Concurrent.Async
-import qualified Control.Event.Handler as Handler
-import Control.Exception
-import Control.Concurrent
-import Data.Maybe (isJust)
-import Text.Parsec.Char
+import Reactive.Banana
+import Reactive.Banana.Frameworks hiding
+    ( register )
 import Text.Parsec
-import Data.Monoid
 
 newtype SentMessage = SentMessage (Integer, String)
 
-newtype Client = Client (Integer, Socket)
+newtype Client = Client (Integer, String, Socket)
 
 newtype EvHandler a = EvHandler (Handler.AddHandler a, Handler.Handler a)
 
@@ -30,6 +26,7 @@ newtype TextMessage = TextMessage String
 data Cmd = CmdQuit
          | CmdHelp
          | CmdBroadcast String
+         | CmdWho
          | CmdError
 
 -- | This class modells data that is associated with a user.
@@ -40,7 +37,13 @@ instance User SentMessage where
     userId (SentMessage (uid,_)) = uid
 
 instance User Client where
-    userId (Client (uid,_)) = uid
+    userId (Client (uid, _, _)) = uid
+
+class Named a where
+    name :: a -> String
+
+instance Named Client where
+    name (Client (_,n,_)) = n
 
 class Message a where
     messageText :: a -> String
@@ -58,7 +61,7 @@ class NetworkTarget a where
     targetSocket :: a -> Socket
 
 instance NetworkTarget Client where
-    targetSocket (Client (_,s)) = s
+    targetSocket (Client (_,_,s)) = s
 
 instance NetworkTarget Socket where
     targetSocket = id
@@ -91,6 +94,7 @@ class ChatCommand a where
     getDirectionalMessage :: a -> Maybe (Integer, String)
     isHelp :: a -> Bool
     isError :: a -> Bool
+    isWho :: a -> Bool
 
 instance ChatCommand Cmd where
     isQuit CmdQuit = True
@@ -102,6 +106,8 @@ instance ChatCommand Cmd where
     isHelp _ = False
     isError CmdError = True
     isError _ = False
+    isWho CmdWho = True
+    isWho _ = False
 
 instance ChatCommand TextMessage where
     getBroadcastMessage (TextMessage s) = getBroadcastMessage $ parseCmd s
@@ -109,6 +115,7 @@ instance ChatCommand TextMessage where
     isHelp (TextMessage s) = isHelp $ parseCmd s
     isError (TextMessage s) = isError $ parseCmd s
     isQuit (TextMessage s) = isQuit $ parseCmd s
+    isWho (TextMessage s) = isWho $ parseCmd s
 
 instance ChatCommand SentMessage where
     getBroadcastMessage (SentMessage (_,s)) = getBroadcastMessage $ parseCmd s
@@ -116,10 +123,26 @@ instance ChatCommand SentMessage where
     isHelp (SentMessage (_,s)) = isHelp $ parseCmd s
     isError (SentMessage (_,s)) = isError $ parseCmd s
     isQuit (SentMessage (_,s)) = isQuit $ parseCmd s
+    isWho (SentMessage (_,s)) = isWho $ parseCmd s
+
+-- | This class modells data that can be force to evaluate
+-- (strictness).
+class Forceable a where
+    force :: a -> ()
+
+instance Forceable a => Forceable [a] where
+    force [] = ()
+    force (x:xs) = force x `seq` force xs
+
+instance Forceable SentMessage where
+    force (SentMessage (uid,m)) = uid `seq` m `seq` ()
+
+instance Forceable Client where
+    force (Client (uid,n,sock)) = uid `seq` n `seq` sock `seq` ()
 
 parseCmd :: String -> Cmd
 parseCmd text =
-    case parse ( do res <-eventually command `alternative` broadcast
+    case parse ( do res <- eventually command `alternative` broadcast
                     _ <- Text.Parsec.many anyChar
                     return res
                ) "" text of
@@ -128,27 +151,35 @@ parseCmd text =
     where
       alternative = (Text.Parsec.<|>)
       eventually = Text.Parsec.try
-      broadcast = do CmdBroadcast <$> Text.Parsec.many anyChar
-      command = do _ <- char '/'
-                   eventually quit `alternative` help
+      broadcast = do beginChar <- noneOf "/"
+                     rest <- Text.Parsec.many anyChar
+                     return (CmdBroadcast (beginChar:rest))
+      command = char '/' >>
+                ( eventually quit `alternative` eventually help `alternative` who )
       help = string "help" >> return CmdHelp
       quit = string "quit" >> return CmdQuit
+      who = string "who" >> return CmdWho
 
 helpText :: String
 helpText =
     unlines [ "Help:"
             , "\t/help: Show this message"
             , "\t/quit: Quit this program"
+            , "\t/who: Show who is online"
             ]
 
 -- | Execute the corresponding io action to a chat command.
-execChatCommand :: (ChatCommand c, User c, User u, NetworkTarget u, Foldable f) =>
+execChatCommand :: ( ChatCommand c, User c
+                   , User u, NetworkTarget u, Named u
+                   , Foldable f) =>
                    f u -> c -> IO ()
 execChatCommand users chatCmd = do
   when (isHelp chatCmd) (sendMessage messageSock (TextMessage $ helpText))
-  when (isQuit chatCmd) (do close messageSock
+  when (isQuit chatCmd) (do sendMessage messageSock $ TextMessage "Goodbye :)\n"
+                            disconnect messageSock
                             putStrLn $ "User "++show (userId chatCmd)++" disconnected"
                         )
+  when (isWho chatCmd) (sendMessage messageSock (TextMessage $ who users))
   case getBroadcastMessage chatCmd of
     Nothing -> return ()
     Just s -> broadcastMessage sendMessage users (SentMessage (userId chatCmd,s))
@@ -157,6 +188,11 @@ execChatCommand users chatCmd = do
             (error "execChatCommand: cannot associate message with any user")
             targetSocket
             (find (\x -> userId x == userId chatCmd) users)
+        who xs =
+            foldl
+            (\s user -> s ++ show (userId user) ++ "\t" ++ (name user) ++ "\n")
+            "ID\tNAME\n"
+            xs
 
 -- | Delete a user from the userlist when they disconnect.
 handleQuit :: ( ChatCommand c, User c
@@ -206,13 +242,16 @@ readN :: (NetworkTarget u) =>
          Int -> u -> IO String
 readN n user = recv (targetSocket user) n
 
+disconnect :: (NetworkTarget u) =>
+              u -> IO ()
+disconnect user =
+    close (targetSocket user)
 
 -- | Poll a listening socket for new connections and trigger an event
 -- handler when a connection occurs.
 runSocketHandler :: (EventHandler h, NetworkTarget a) =>
                     a -> (Socket -> IO b) -> h b -> IO ThreadId
 runSocketHandler target f handler = do
-  putStrLn "Installing socket handler"
   ok <- isListening sock
   when (not ok) (fail "runSocketHandler: socket not listening")
   forkIO $ go sock
@@ -226,13 +265,15 @@ runSocketHandler target f handler = do
       sock = targetSocket target
 
 -- | Execute a IO action and issue a callback, when the action was
--- performed.
-withCallbackDo :: (EventHandler h) =>
+-- performed.  This prcedure spawnes an asyncronous computation that
+-- is only blocking in case of unsafe calls.
+withCallbackDo :: (EventHandler h, Forceable b, Forceable u) =>
                   u -> (u -> IO b) -> h u -> h b -> IO (Async ())
 withCallbackDo user action userReady resultReady =
-    async $ do result <- action user
+    async ( do result <- force user `seq` action user
                fire resultReady result
                fire userReady user
+          )
 
 getId :: Event t (Integer -> a) -> Event t a
 getId ev =
@@ -248,37 +289,62 @@ main = do
   connectEvents <- (newEventHandler :: IO (EvHandler Socket))
   messageEvents <- (newEventHandler :: IO (EvHandler SentMessage))
   userReady <- (newEventHandler :: IO (EvHandler Client))
-  thread <- runSocketHandler sock return connectEvents
+  _ <- runSocketHandler sock return connectEvents
   network <- compile $ do
                newConnections <- getEvents connectEvents
                userReadyEvent <- getEvents userReady
                newMessages <- getEvents messageEvents
-               let users = accumB [] ( unions [ userAdd
+               let -- Behavior
+                   users = accumB [] ( unions [ userAdd
                                               , userQuit
                                               ]
                                      )
+                   -- Events
                    userAdd = (:) <$> newUser
-                   userQuit = flip handleQuit <$> newMessages
-                   newUser = getId ((\sct uid -> Client (uid,sct)) <$> newConnections)
+                   userQuit = flip handleQuit <$> quittingUser
+                   quittingUser = filterE isQuit newMessages
+                   newUser = getId ((\sct uid -> Client (uid,"<incognito>",sct)) <$> newConnections)
+                   commandErrors = filterE isError newMessages
+                   -- Functions
                    greet user = do
                          putStrLn $ "User connected, user id is "++show (userId user)
                          sendMessage user $ TextMessage (greeting user)
                          fire userReady user
-                   greeting user = unlines [ "Hello", "Your id is "++show (userId user),"Happy Chatting" ]
+                   greeting user =
+                       unlines [ "Hello"
+                               , "Your id is "++show (userId user)
+                               , "Happy Chatting"
+                               ]
                    getMessage user = do
                          msg <- readN 100 user
                          return (SentMessage (userId user, msg))
                reactimate $ greet <$> newUser
                reactimate $ (\user -> do
-                               withCallbackDo
-                                   user
-                                   getMessage
-                                   userReady
-                                   messageEvents
+                               _ <- withCallbackDo
+                                    user
+                                    getMessage
+                                    userReady
+                                    messageEvents
                                return ()
                             ) <$> userReadyEvent
                reactimate $ execChatCommand <$> users <@> newMessages
+               reactimate $ (\targets leavingUser ->
+                                 mapM_
+                                 (flip sendMessage $ TextMessage $
+                                       show (userId leavingUser) ++" has left the chat\n")
+                                 (filter (\targ -> userId targ /= userId leavingUser) targets)
+                            ) <$> users <@> quittingUser
+--               reactimate $ (putStr.messageText) <$> newMessages
+               reactimate $ (\curUsers message ->
+                                 case find (\user -> userId user == userId message) curUsers of
+                                   Just x -> sendMessage
+                                             x
+                                             ( TextMessage ("***Syntax error\n"++helpText))
+                                   Nothing -> return ()
+                            ) <$>
+                            users <@>
+                            commandErrors
   actuate network
-  _ <- forever $ threadDelay (10*10^6)
+  _ <- forever $ threadDelay (10*10^(6 :: Int))
   return ()
     where port = 4141
